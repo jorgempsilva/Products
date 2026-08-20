a965acbea52ddf2aa00aebef5abed3c48a6577e6
Jorge Silva <jorgempsilva97@gmail.com>
2026-08-20 15:45:56 +0100

    chore: remove unused ProductIdExhaustedException (#18)

--- src/Products.Domain/Exceptions/ProductIdExhaustedException.cs
+++
@@ -1,9 +0,0 @@
-namespace Products.Domain.Exceptions;
-
-public sealed class ProductIdExhaustedException : DomainException
-{
-    public ProductIdExhaustedException()
-        : base("The product id range (100000-999999) has been exhausted.")
-    {
-    }
-}
